59e1b1ad0c2bccd47e72f0f10d482be14086149b
paulo.martins <paulo.martins@D_007598.demprel.local>
2022-03-01 16:35:17 -0300

    Iniciando o projeto

--- apicpf/src/main/java/br/gov/emprel/controllers/CpfController.java
+++ apicpf/src/main/java/br/gov/emprel/controllers/CpfController.java
@@ -1,7 +1,23 @@
 package br.gov.emprel.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.gov.emprel.models.CpfUsuario;
+
+@RestController
+@RequestMapping("/emprel")
 public class CpfController {
+	
+	@Autowired
+	private CpfUsuario usuario;
+	
+	@GetMapping
+	public String validarCpf() {
+		return (usuario.getResultado());
+	}
 	
 
 }
