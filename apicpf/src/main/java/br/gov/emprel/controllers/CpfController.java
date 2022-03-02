9740530655968b0aae23a7fe06776c512f677d7b
paulo.martins <paulo.martins@D_007598.demprel.local>
2022-03-02 14:28:20 -0300

    api funcional

--- apicpf/src/main/java/br/gov/emprel/controllers/CpfController.java
+++ apicpf/src/main/java/br/gov/emprel/controllers/CpfController.java
@@ -1,23 +1,28 @@
 package br.gov.emprel.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.emprel.models.CpfUsuario;
 
+
 @RestController
 @RequestMapping("/emprel")
 public class CpfController {
 	
-	@Autowired
-	private CpfUsuario usuario;
+
 	
-	@GetMapping
-	public String validarCpf() {
-		return (usuario.getResultado());
+	//Método validar cpf
+	@GetMapping("{cpf}")
+	public String validarCpf(@PathVariable String cpf) {
+		
+		CpfUsuario usuario = new CpfUsuario();
+		
+		return usuario.validarCpf(cpf);
+		
+
 	}
-	
 
 }
--- apicpf/src/main/java/br/gov/emprel/models/CpfUsuario.java
+++ apicpf/src/main/java/br/gov/emprel/models/CpfUsuario.java
@@ -1,95 +1,95 @@
 package br.gov.emprel.models;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class CpfUsuario {
 	
 	//Atributos
-	private String cpf = "07218050786";
-	private int soma1 = 0;
-	private int soma2 = 0;
-	private int resto1;
-	private int resto2;
-	private int validador1;
-	private int validador2;
-	private int multiplica1 = 1;
-	private int multiplica2 = 1;
-	private int i;
-	private int j;
-	private int x = 0;
-	private String resultado;
-	
-	
-	
-	public String getResultado() {
-		return resultado;
-	}
-
-
-
-	public void setResultado(String resultado) {
-		this.resultado = resultado;
-	}
-
-
-
-	//Método validar cpf
-	public void validarCpf() {
-		String[] conv = this.cpf.split("");
+		private String resultado;
+		private int soma1 = 0;
+		private int soma2 = 0;
+		private int resto1;
+		private int resto2;
+		private int validador1;
+		private int validador2;
+		private int multiplica1 = 1;
+		private int multiplica2 = 1;
+		private int i;
+		private int j;
+		private int x = 0;
 		
-	//Digitos validadores
-	validador1 = Integer.parseInt(conv[9]);
-	validador2 = Integer.parseInt(conv[10]);
-				
-	//Valida primeiro dígito
-	for(i=0;i<(this.cpf.length()-2);i++) {
+		
+		//Getters and Setters
+		public String getResultado() {
+			return resultado;
+		}
+
+		public void setResultado(String resultado) {
+			this.resultado = resultado;
+		}
+
+
+
+		//Método validar cpf
+		public String validarCpf(String cpf) {
+		String[] conv = cpf.split("");
+		
+		
+		//Digitos validadores
+		validador1 = Integer.parseInt(conv[9]);
+		validador2 = Integer.parseInt(conv[10]);
+
+		
+		//Valida primeiro dígito
+		for(i=0;i<(cpf.length()-2);i++) {
 		soma1 = soma1 + (i+1)*Integer.parseInt(conv[i]);
-	}
-				
-	resto1 = soma1%11;
-				
-	if(resto1 == 10) {
+		}
+
+		resto1 = soma1%11;
+
+		if(resto1 == 10) {
 		resto1 = 0;
-	} 
-				
-	//Valida segundo dígito
-	for(i=0;i<(this.cpf.length()-1);i++) {
+		}
+
+		
+		//Valida segundo dígito
+		for(i=0;i<(cpf.length()-1);i++) {
 		soma2 = soma2 + (i)*Integer.parseInt(conv[i]);
-	}
-				
-	resto2 = soma2%11;
-				
-	if(resto2 == 10) {
+		}
+
+		resto2 = soma2%11;
+
+		if(resto2 == 10) {
 		resto2 = 0;
-	} 
-				
-	//Método verificar se CPF é válido
-				
-	//Escolhe o primeiro algarismo diferente de zero
-	while(x == 0) {
+		}
+
+		
+		//Método verificar se CPF é válido
+
+		//Escolhe o primeiro algarismo diferente de zero
+		while(x == 0) {
 		x = Integer.parseInt(conv[j]);
 		j++;
-	}
-				
-	//Verifica se usuário não colocou todos os algarismo iguais
-	for(i=0;i<this.cpf.length();i++) {
+		}
+
+		
+		//Verifica se usuário não colocou todos os algarismo iguais
+		for(i=0;i<cpf.length();i++) {
 		multiplica1 = (multiplica1)*(Integer.parseInt(conv[i]));
 		multiplica2 = multiplica2*(Integer.parseInt(conv[j-1]));
-	}
-				
-				
-	//Verificar se o cpf é valido
-	if(multiplica1 == multiplica2) {
-		resultado = "CPF inválido";
-	} else if(resto1 == validador1 && resto2 == validador2) {
-		resultado = "CPF valido";			
-	} else {
-		resultado = "CPF inválido";
-	}
-	
-}	
+		}
 
-	
+
+		//Verificar se o cpf é valido
+		if(multiplica1 == multiplica2) {
+		resultado = "CPF inválido";
+		} else if(resto1 == validador1 && resto2 == validador2) {
+		resultado = "CPF valido";
+		} else {
+		resultado = "CPF inválido";
+		}
+		System.out.println(resultado);
+		return resultado;
+		
+		}
 
 }
